4b0dfd8a69b399c8b77d64e0b271b0e916850aa6
webdesarrollo <webdesarrollo2@gmail.com>
2018-08-26 10:09:48 -0300

    return to pagination
    pagination return test

--- PaginationServiceProvider.php
+++ PaginationServiceProvider.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace DevNoisy\Manage;
+//namespace Illuminate\Pagination;
+namespace Sejo\Pagination;
 
 use Illuminate\Support\ServiceProvider;
 
-class ManageServiceProvider extends ServiceProvider
+class PaginationServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
@@ -13,12 +14,12 @@ class ManageServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/src/views', 'manage');//src o resources
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'pagination');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/src/views' => $this->app->resourcePath('views/vendor/pagination'),
-            ], 'DevNoisy');
+                __DIR__.'/resources/views' => $this->app->resourcePath('views/vendor/pagination'),
+            ], 'sejo-laravel-pagination');/*laravel-pagination*/
         }
     }
 
@@ -28,7 +29,7 @@ class ManageServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    {   
-        //
+    {
+
     }
 }
